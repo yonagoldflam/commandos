535cdd8ab11b03550d8aa7d8d557305a05cc75e9
tzirkus <zalmen5779@gmail.com>
2025-05-27 13:15:24 +0300

    Abstraction

--- commandos/Program.cs
+++ commandos/Program.cs
@@ -4,12 +4,14 @@ namespace commandos
 {
     internal class Program
     {
-        public static Soldier damen = new Soldier("Itzchok Damen", 2355144, "hiding");
+        public static Commando damen = new Commando("Itzchok Damen", "izik", "hiding");
         public static Weapon M16 = new Weapon("m16", "USA", 62);
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"name: {damen.Name}. id: {damen.Id}. tools: {damen.Tools}. status: {damen.Name}");
+            Console.WriteLine(damen.CodeName);
+            damen.CodeName = "Izik";
+            Console.WriteLine($"name: {damen.SayName("general")}. code name: {damen.CodeName}. tools: {damen.Tools[4]}. status: {damen.Status}");
             Console.WriteLine($"name: {M16.Name}. maker: {M16.Maker}. sum balls: {M16.SumBalls}");
         }
     }
