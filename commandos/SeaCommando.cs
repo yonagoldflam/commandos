119ca4ebcbd0791afa9ef3b78966681090b53ddb
tzirkus <zalmen5779@gmail.com>
2025-05-27 14:31:39 +0300

    AirCommando & SeaCommando

---
+++ commandos/SeaCommando.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandos
+{
+    public class SeaCommando : Commando
+    {
+        public SeaCommando(string Name, string code_name, string Status) : base(Name, code_name, Status)
+        {
+
+        }
+
+
+        public void Swimming()
+        {
+            Console.WriteLine("Swimming was performed");
+        }
+    }
+}
