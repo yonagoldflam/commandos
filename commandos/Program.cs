84d9da0caa4e23e3972e802e1b001713212160ae
tzirkus <zalmen5779@gmail.com>
2025-05-27 11:41:09 +0300

    soldier

---
+++ commandos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace commandos
+{
+    internal class Program
+    {
+        public static Soldier damen = new Soldier("Itzchok Damen", 2355144, "hiding");
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"name: {damen.Name}. id: {damen.Id}. tools: {damen.Tools}. status: {damen.Name}");
+        }
+    }
+
+}
---
+++ commandos/Soldier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandos
+{
+    public class Soldier
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string[] Tools { get; set; } = { "hammer", "chisel", "rope", "bag", "lantern" };
+        public string Status { get; set; }
+
+
+        public Soldier(string name, int id, string status)
+        {
+            Name = name.ToLower();
+            Id = id;
+            Status = status.ToLower();           
+        }
+
+
+        public void Walk()
+        {
+            Status = "walk";
+            Console.WriteLine("the soldier is walk");
+        }
+
+
+        public void Hide()
+        {
+            Status = "hiding";
+            Console.WriteLine("the soldier is hiding");
+        }
+
+
+        public void Attack()
+        {
+            //נצמד להוראות ולא משנה סטטוס
+            Console.WriteLine($"soldier {Name} is attack");
+        }
+    }
+}
